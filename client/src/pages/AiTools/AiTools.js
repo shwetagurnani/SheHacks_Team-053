690957ed9a327ab686627e9e99ec6bc167bdc8f7
shreyaag770 <agarwalshreya770@gmail.com>
2021-04-17 02:58:38 +0530

    fixed ui of cervical

--- client/src/pages/AiTools/AiTools.js
+++ client/src/pages/AiTools/AiTools.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { makeStyles } from "@material-ui/core/styles";
-import { Link} from "@material-ui/core";
+import { Link } from "react-router-dom";
 import Button from "@material-ui/core/Button";
 // import BackgroundHeader from "./public/Bg1.png";
 
@@ -153,11 +153,12 @@ const AiTools = () => {
               <div className={classes.subHeading}>
               Benign tumors do not invade the surrounding tissues and are therefore non-cancerous while Malignant tissues are cancerous and can invade the surrounding tissues. Benign tumours can be treated with surgery and are easy to temove while malignant tumours are difficult to remove but can be treated with chemotherapy, radiation therapy or immunotherapy treatments.
               </div>
-              
-              <Button className={classes.button} variant="contained"  href="/breastcancer">
+              <Link to="/breastcancer" style={{textDecoration: 'none'}}>
+              <Button className={classes.button} variant="contained">
                 
                 Detect Now
               </Button>
+              </Link>
            
               
             </div>
@@ -174,10 +175,11 @@ const AiTools = () => {
               Cervical cancer is a malignant tumour of the cervix, the lowermost part of the uterus.This cancer affects the deeper tissues of the cervix and can spread to other parts of the body including lung,liver,bladder,vagina and rectum.It is observed that people with other common diseases like Sexually Transmitted disease,HIV/AIDS etc. develop cervical cancer.
 Please answer the following questions to know if you have cervical cancer using the Machine Learning model which was trained over the data with results of cervical cancer using tests like Hinselmann,Schiller,Cytology and Biopsy.
               </div>
-              
-              <Button className={classes.button} variant="contained" href="/cervical">
+              <Link to="/cervical" style={{textDecoration: 'none'}}>
+              <Button className={classes.button} variant="contained">
                 Detect Now
               </Button>
+              </Link>
             
               
             </div>
@@ -194,11 +196,11 @@ Please answer the following questions to know if you have cervical cancer using 
               Polycystic ovary syndrome (PCOS) is a condition that affects a woman's hormone levels.1 out of every 5 women suffer from PCOS.The cause of polycystic ovary syndrome isn't well understood, but may involve a combination of genetic and environmental factors.Still there is lack of awareness among women about PCOS and it is not detected until they are taken to a doctor.
  Please answer the questionnaire below to know if u are at the risk of PCOS.
               </div>
-              
-              <Button className={classes.button} variant="contained"
-              href="/pcos">
+              <Link to="/pcos" style={{textDecoration: 'none'}}>
+              <Button className={classes.button} variant="contained">
                 Detect Now
               </Button>
+              </Link>
              
             </div>
           </div>
